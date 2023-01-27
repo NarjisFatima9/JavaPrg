0818561d54d21fdcb96c81fdbcfdcbcc5a0a1d7c
NarjisFatima9 <123392154+NarjisFatima9@users.noreply.github.com>
2023-01-26 19:37:41 -0600

    Add files via upload

---
+++ PrjAbsClass/Main.java
@@ -0,0 +1,7 @@
+/*object is provided through method e.g. getShape() method*/
+public class Main {
+            public static void main(String args[]){
+            Shape s=new Circle();
+            s.draw();
+        }
+}
---
+++ PrjAbsClass/Shape.java
@@ -0,0 +1,5 @@
+/*Abstract Class and method*/
+
+abstract class Shape {
+            abstract void draw();
+    }
